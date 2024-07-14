57ad31aa36b667819fc187b517b6ecb80d07ed5f
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-14 17:16:06 +0900

    progress bar

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -51,13 +51,37 @@ const VideoCarousel = () => {
   const handleLoadedMetadata = (i, e) => setloadedData((pre) => [...pre, e]);
 
   useEffect(() => {
-    const currentProgress = 0;
+    let currentProgress = 0;
     let span = videoSpanRef.current;
 
     if (span[videoId]) {
       let anim = gsap.to(span[videoId], {
-        onUpdate: () => {},
-        onComplete: () => {},
+        onUpdate: () => {
+          const progress = Math.ceil(anim.progress() * 100);
+          if (progress != currentProgress) {
+            currentProgress = progress;
+
+            gsap.to(videoDivRef.current[videoId], {
+              width:
+                window.innerWidth < 760
+                  ? "10vw"
+                  : window.innerWidth < 1200
+                  ? "10vw"
+                  : "4vw",
+            });
+            gsap.to(span[videoId], {
+              width: `${currentProgress}%`,
+              backgroundColor: "white",
+            });
+          }
+        },
+        onComplete: () => {
+          if (isPlaying) {
+            gsap.to(videoDivRef.current[videoId], {
+              width: "12px",
+            });
+          }
+        },
       });
     }
   }, [videoId, startPlay]);
@@ -92,7 +116,6 @@ const VideoCarousel = () => {
                   playsInline={true}
                   preload="auto"
                   muted
-                  loop
                   ref={(el) => (videoRef.current[i] = el)}
                   onPlay={() => {
                     setVideo((prevVideo) => ({
