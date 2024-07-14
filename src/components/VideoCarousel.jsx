4f31a9b5691cb231b785dad3e948059ae36d0b47
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-14 17:23:39 +0900

    update

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -80,9 +80,25 @@ const VideoCarousel = () => {
             gsap.to(videoDivRef.current[videoId], {
               width: "12px",
             });
+            gsap.to(span[videoId], {
+              backgroundColor: "#afafaf",
+            });
           }
         },
       });
+      if (videoId === 0) {
+        anim.restart();
+      }
+      const animUpdate = () => {
+        anim.progress(
+          videoRef.current[videoId] / hightlightsSlides[videoId].videoDuration
+        );
+      };
+      if (isPlaying) {
+        gsap.ticker.add(animUpdate);
+      } else {
+        gsap.ticker.remove(animUpdate);
+      }
     }
   }, [videoId, startPlay]);
 
