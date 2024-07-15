aaccc2b3aa3a3f4bf9309f7a4764403d25334bc0
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-15 16:50:11 +0900

    under img

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -71,6 +72,12 @@ const Model = () => {
             >
               <View.Port />
             </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">{models.map()}</ul>
+            </div>
           </div>
         </div>
       </div>
