06835df0d867afdd56af8c6d3af7f16b44309a18
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-15 16:56:10 +0900

    model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -76,7 +76,18 @@ const Model = () => {
           <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
             <div className="flex-center">
-              <ul className="color-container">{models.map()}</ul>
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2"
+                    style={{
+                      backgroundColor: item.color[0],
+                    }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
             </div>
           </div>
         </div>
