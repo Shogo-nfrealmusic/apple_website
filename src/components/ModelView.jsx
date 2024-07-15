09a5334e99ddde9428fafcb4e3783d20b02c2838
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-15 22:33:36 +0900

    good

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,8 +4,10 @@ import {
   PerspectiveCamera,
   View,
 } from "@react-three/drei";
+
 import * as THREE from "three";
 import Lights from "./Lights";
+import Loader from "./Loader";
 import IPhone from "./IPhone";
 import { Suspense } from "react";
 
@@ -14,7 +16,7 @@ const ModelView = ({
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -22,8 +24,9 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
 
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
@@ -42,19 +45,15 @@ const ModelView = ({
 
       <group
         ref={groupRef}
-        name={`${index === 1} ? 'small' :
-      'large'
-      `}
+        name={`${index === 1} ? 'small' : 'large`}
         position={[0, 0, 0]}
       >
-        <Suspense
-          fallback={
-            <Html>
-              <div>Loading</div>
-            </Html>
-          }
-        >
-          <IPhone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} />
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
         </Suspense>
       </group>
     </View>
