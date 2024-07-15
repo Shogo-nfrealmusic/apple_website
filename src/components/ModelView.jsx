36349e7e6bdbe3c902cd1420195e3e855a987ae4
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-15 18:13:43 +0900

    turorial

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,21 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { View } from "@react-three/drei";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full s{index === 2} ? 'right-[-100%] : ''`}
+    ></View>
+  );
 };
 
 export default ModelView;
