3c8b98477f6975363783a165d637adf71d3b1539
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-15 18:05:03 +0900

    button

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -80,7 +80,7 @@ const Model = () => {
                 {models.map((item, i) => (
                   <li
                     key={i}
-                    className="w-6 h-6 rounded-full mx-2"
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
                     style={{
                       backgroundColor: item.color[0],
                     }}
@@ -88,6 +88,21 @@ const Model = () => {
                   />
                 ))}
               </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
           </div>
         </div>
