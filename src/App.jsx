f52592f172486dcb26340797a2c0f075c2ab39e1
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-16 17:34:31 +0900

    sentry setUp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -14,4 +15,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
