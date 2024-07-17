152cff8bfb18d3f2bc2178ae417ee967feb3bc5c
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-17 18:18:06 +0900

    video

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { chipImg } from "../utils";
+import React, { useRef } from "react";
+import { chipImg, frameImg, frameVideo } from "../utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const HowItWorks = () => {
+  const videoRef = useRef();
   useGSAP(() => {
     gsap.from("#chip", {
       scrollTrigger: {
@@ -21,6 +22,39 @@ const HowItWorks = () => {
       <div className="screen-max-width">
         <div id="chip" className="flex-center w-full my-20">
           <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            A17 Pro chip.
+            <br />A monster win for gaming.
+          </h2>
+          <p className="hiw-subtitle">
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+        <div className="mt-10 md:mt-20 mb-14">
+          <div className="relative h-full flex-center">
+            <div className="overflow-hidden">
+              <img
+                src={frameImg}
+                alt="frame"
+                className="bg-transparent relative z-10"
+              />
+            </div>
+            <div className="hiw-video">
+              <video
+                className="pointer-events-none"
+                playsInline
+                preload="none"
+                muted
+                autoPlay
+                loop
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
