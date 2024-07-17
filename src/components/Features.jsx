7e7946c367d73a51b520bdb91259cc6804b22b47
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-17 17:58:06 +0900

    finish feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
 import { animateWithGsap } from "../utils/animations";
 import { explore1Img, explore2Img, exploreVideo } from "../utils";
-import { useRef } from "react";
 import gsap from "gsap";
 
 const Features = () => {
   const videoRef = useRef();
+
   useGSAP(() => {
     gsap.to("#exploreVideo", {
       scrollTrigger: {
@@ -17,17 +18,11 @@ const Features = () => {
         videoRef.current.play();
       },
     });
-    animateWithGsap("#features_title", {
-      y: 0,
-      opacity: 1,
-    });
+
+    animateWithGsap("#features_title", { y: 0, opacity: 1 });
     animateWithGsap(
       ".g_grow",
-      {
-        scale: 1,
-        opacity: 1,
-        ease: "power1",
-      },
+      { scale: 1, opacity: 1, ease: "power1" },
       { scrub: 5.5 }
     );
     animateWithGsap(".g_text", {
@@ -37,21 +32,24 @@ const Features = () => {
       duration: 1,
     });
   }, []);
+
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-width">
+      <div className="screen-max-wdith">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">
             Explore the full story.
           </h1>
         </div>
+
         <div className="flex flex-col justify-center items-center overflow-hidden">
           <div className="mt-32 mb-24 pl-24">
-            <h2 className="text-5xl lg: text-7xl font-semibold">iPhone.</h2>
-            <h2 className="text-5xl lg: text-7xl font-semibold">
-              Forged in Titanium.
+            <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
+            <h2 className="text-5xl lg:text-7xl font-semibold">
+              Forged in titanium.
             </h2>
           </div>
+
           <div className="flex-center flex-col sm:px-10">
             <div className="relative h-[50vh] w-full flex items-center">
               <video
@@ -66,6 +64,7 @@ const Features = () => {
                 <source src={exploreVideo} type="video/mp4" />
               </video>
             </div>
+
             <div className="flex flex-col w-full relative">
               <div className="feature-video-container">
                 <div className="overflow-hidden flex-1 h-[50vh]">
@@ -78,15 +77,16 @@ const Features = () => {
                 <div className="overflow-hidden flex-1 h-[50vh]">
                   <img
                     src={explore2Img}
-                    alt="titanium2"
+                    alt="titanium 2"
                     className="feature-video g_grow"
                   />
                 </div>
               </div>
+
               <div className="feature-text-container">
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
-                    iPhone 15 Pro
+                    iPhone 15 Pro is{" "}
                     <span className="text-white">
                       the first iPhone to feature an aerospace-grade titanium
                       design
@@ -95,10 +95,11 @@ const Features = () => {
                     Mars.
                   </p>
                 </div>
+
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
                     Titanium has one of the best strength-to-weight ratios of
-                    any metal, making these our
+                    any metal, making these our{" "}
                     <span className="text-white">
                       lightest Pro models ever.
                     </span>
